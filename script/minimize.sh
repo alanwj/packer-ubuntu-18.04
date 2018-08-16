f366670ed7688eb4cb5e59ef04d0d3e93b24b2da
Alan Johnson <alan.wayne.johnson@gmail.com>
2018-08-15 21:38:51 -0700

    Don't remove APT package information during minimization.
    This allows apt install to work correctly on new vagrant boxes.

--- script/minimize.sh
+++ script/minimize.sh
@@ -49,8 +49,6 @@ apt-get -y purge deborphan dialog
 
 echo "==> Removing man pages"
 rm -rf /usr/share/man/*
-echo "==> Removing APT files"
-find /var/lib/apt -type f | xargs rm -f
 echo "==> Removing any docs"
 rm -rf /usr/share/doc/*
 echo "==> Removing caches"
